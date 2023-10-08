37f8a39f281da4cbae52930002f6b284ace01cb6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-08 19:12:04 +0200

    implemented size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,14 @@ import { useState } from "react";
 export default function Home() {
   const { canvasRef, onMouseDown } = useDraw(drawLine);
   const [color, setColor] = useState("#000");
+  const [size, setSize] = useState<number | string>(0);
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
     const { x: currX, y: currY } = currentPoint;
-    const lineColor = "#000";
-    const lineWidth = 2;
 
     let startPoint = prevPoint ?? currentPoint;
 
     ctx.beginPath();
-    ctx.lineWidth = lineWidth;
+    ctx.lineWidth = size;
     ctx.strokeStyle = color;
 
     ctx.moveTo(startPoint.x, startPoint.y);
@@ -24,7 +23,7 @@ export default function Home() {
 
     ctx.fillStyle = color;
     ctx.beginPath();
-    ctx.arc(startPoint.x, startPoint.y, 1, 0, 4 * Math.PI);
+    ctx.arc(startPoint.x, startPoint.y, size, 0, 4 * Math.PI);
 
     ctx.fill();
     ctx.closePath();
@@ -32,6 +31,15 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
       <SketchPicker color={color} onChange={(e) => setColor(e.hex)} />
+      <input
+        type="range"
+        min={0}
+        max={10}
+        step={1}
+        value={size}
+        onChange={(e) => setSize((prev) => (prev = e.target.value))}
+      />
+      size: {size}
       <canvas
         width={720}
         height={720}
