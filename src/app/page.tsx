ef0f4bafed0252cf35c8c641a499185824f017b9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-08 19:07:48 +0200

    working paint!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,43 @@
 "use client";
 import Image from "next/image";
 import { useDraw } from "../hooks/useDraw";
-
+import { SketchPicker } from "react-color";
+import { useState } from "react";
 export default function Home() {
-  const { canvasRef } = useDraw();
-
+  const { canvasRef, onMouseDown } = useDraw(drawLine);
+  const [color, setColor] = useState("#000");
   function drawLine({ prevPoint, currentPoint, ctx }: Draw) {
     const { x: currX, y: currY } = currentPoint;
-    const color = "#000";
-    const lineWidth = 5;
+    const lineColor = "#000";
+    const lineWidth = 2;
+
+    let startPoint = prevPoint ?? currentPoint;
+
+    ctx.beginPath();
+    ctx.lineWidth = lineWidth;
+    ctx.strokeStyle = color;
+
+    ctx.moveTo(startPoint.x, startPoint.y);
+
+    ctx.lineTo(currX, currY);
+    ctx.stroke();
+
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(startPoint.x, startPoint.y, 1, 0, 4 * Math.PI);
+
+    ctx.fill();
+    ctx.closePath();
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
+      <SketchPicker color={color} onChange={(e) => setColor(e.hex)} />
       <canvas
-        width={750}
-        height={750}
+        width={720}
+        height={720}
         className="border-2 border-gray-900"
         ref={canvasRef}
+        onMouseDown={onMouseDown}
       />
     </main>
   );
--- src/hooks/useDraw.ts
+++ src/hooks/useDraw.ts
@@ -1,33 +1,47 @@
 
-import { useEffect, useRef } from "react"
+import { useEffect, useRef, useState } from "react"
 export const useDraw = (onDraw: ({ ctx, currentPoint, prevPoint }: Draw) => void) => {
+    const [isMouseDown, setIsMouseDown] = useState(false)
     const canvasRef = useRef<HTMLCanvasElement>(null)
+    const prevPoint = useRef<null | Point>(null)
+
+    const onMouseDown = () => setIsMouseDown(true)
 
 
     useEffect(() => {
 
         const handler = (e: MouseEvent) => {
+            if (!isMouseDown) return
             const currentPoint = computePointInCanvas(e);
-            console.log(currentPoint)
             const ctx = canvasRef.current?.getContext("2d")
             if (!ctx || !currentPoint) return
 
-        }
+            onDraw({ ctx, currentPoint, prevPoint: prevPoint.current })
+            prevPoint.current = currentPoint
 
+        }
+        const mouseUpHandler = () => {
+            setIsMouseDown(false)
+            prevPoint.current = null
+        }
         const computePointInCanvas = (e: MouseEvent) => {
             const canvas = canvasRef.current;
             if (!canvas) return
 
             const rect = canvas.getBoundingClientRect();
-            const x = Math.floor(e.clientX - rect.left);
+            const x = e.clientX - rect.left
             const y = e.clientY - rect.top;
 
             return { x, y }
         }
         canvasRef.current?.addEventListener('mousemove', handler)
+        window.addEventListener('mouseup', mouseUpHandler)
+        return () => {
+            canvasRef.current?.removeEventListener('mousemove', handler)
 
-        return () => canvasRef.current?.addEventListener('mousemove', handler)
-    }, [])
+            window.removeEventListener('mouseup', mouseUpHandler)
+        }
+    }, [onDraw])
 
-    return { canvasRef }
+    return { canvasRef, onMouseDown }
 }
